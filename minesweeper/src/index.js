d64c6290107eafc7554b7aa6c1fe964f471685c2
Illia Sakharau <sakharovia0204@gmail.com>
2023-05-14 21:26:05 +0400

    init: start task

---
+++ minesweeper/src/index.js
@@ -0,0 +1,7 @@
+import img from './assets/image.jpg';
+import './style.scss';
+
+console.log('jagckacbhs');
+document.body.innerHTML = `
+  <img src="${img}" alt="123">
+`;
---
+++ minesweeper/webpack.config.js
@@ -0,0 +1,62 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const devServer = (isDev) => !isDev ? {} : {
+  devServer: {
+    open: true,
+    hot: true,
+    port: 3000,
+    // contentBase: path.join(__dirname, 'public'),
+  }
+};
+
+const mapDevtool = (isDev) => !isDev ? {} : {devtool: 'inline-source-map'};
+
+module.exports = ({develop}) => ({
+  mode: develop ? 'development' : 'production',
+  ...mapDevtool(develop),
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    assetModuleFilename: 'assets/[hash][ext]'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Minesweeper',
+      favicon: './public/fav.png'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css',
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: './public', to: './assets' }
+      ]
+    }),
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+  ],
+  ...devServer(develop),
+});
