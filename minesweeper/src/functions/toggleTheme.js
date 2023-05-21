ee10b64cd0781f2a13124515cc16a5dd2a37b9a3
Illia Sakharau <sakharovia0204@gmail.com>
2023-05-21 20:53:04 +0400

    fix: initial theme selection

--- minesweeper/src/functions/toggleTheme.js
+++ minesweeper/src/functions/toggleTheme.js
@@ -4,12 +4,13 @@ function setTheme() {
   const menu = document.querySelector('.menu');
   const area = document.querySelector('.area');
 
-  if (localStorage.getItem('ily-theme') === 'dark') {
-    menu.classList.remove('menu_light'); 
-    area.classList.remove('area_light');
-  } else {
-    menu.classList.add('menu_light'); 
+  if (localStorage.getItem('ily-theme') === 'light') {
+    menu.classList.add('menu_light');
     area.classList.add('area_light');
+  } else {
+    menu.classList.remove('menu_light');
+    area.classList.remove('area_light');
+    
   }
 }
 
