a5b63cf4299297fd3c2c39091a3e6170d0df8aec
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-12 21:12:51 +0400

    feat: migrate src/components/view/appView.js to TS

---
+++ migration-newip-to-ts/src/components/view/appView.ts
@@ -0,0 +1,21 @@
+import { IArticles, IRespArticles, IRespSourses, ISourse } from '../types/index';
+import News from './news/news';
+import Sources from './sources/sources';
+
+export class AppView {
+    public news: News = new News();
+    public sources: Sources = new Sources();
+
+
+    public drawNews(data: IRespArticles): void {
+        const values: IArticles[] = data?.articles ? data?.articles : [];
+        this.news.draw(values);        
+    }
+
+    public drawSources(data: IRespSourses): void {
+        const values: ISourse[] = data?.sources ? data?.sources : [];
+        this.sources.draw(values);
+    }
+}
+
+export default AppView;
