7a51159be3a28c50574b718db055eb3794f75634
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-12 13:43:45 +0400

    feat: add own apiKey

--- migration-newip-to-ts/src/components/controller/appLoader.js
+++ migration-newip-to-ts/src/components/controller/appLoader.js
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     constructor() {
         super('https://newsapi.org/v2/', {
-            apiKey: '', // получите свой ключ https://newsapi.org/
+            apiKey: '0992a14004c34237aa6b899375e6a23e', // получите свой ключ https://newsapi.org/
         });
     }
 }
