a9800e310dfa189cfbb654aa202b780b9baec498
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-12 19:46:47 +0400

    feat: migrate src/components/view/sources/sources.js to TS

---
+++ migration-newip-to-ts/src/components/view/sources/sources.ts
@@ -0,0 +1,31 @@
+import { ISourse } from '../../types/index';
+import './sources.css';
+
+class Sources {
+    draw(data: ISourse[]) {
+        const fragment: DocumentFragment = document.createDocumentFragment();
+        const sourceItemTemp: HTMLTemplateElement | null = document.querySelector('#sourceItemTemp');
+        const sourceElem = document.querySelector('.sources');
+
+        data.forEach((item) => {
+            if (sourceItemTemp !== null) {
+                const sourceClone = sourceItemTemp.content.cloneNode(true) as HTMLElement;
+                const sourceItemName: HTMLElement | null = sourceClone.querySelector('.source__item-name');
+                const sourceItem: HTMLElement | null = sourceClone.querySelector('.source__item');
+
+                if (sourceItemName !== null) {
+                    sourceItemName.textContent = item.name;
+                }
+                if (sourceItem !== null) {
+                    sourceItem.setAttribute('data-source-id', item.id);
+                }
+
+                fragment.append(sourceClone);
+            }
+        });
+
+        sourceElem?.append(fragment);
+    }
+}
+
+export default Sources;
