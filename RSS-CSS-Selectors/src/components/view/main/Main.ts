22126b4a1e909c89b4c11fdcf43d7f28f302eea4
Illia Sakharau <sakharovia0204@gmail.com>
2023-06-22 01:08:16 +0400

    feat: add aside view

--- RSS-CSS-Selectors/src/components/view/main/Main.ts
+++ RSS-CSS-Selectors/src/components/view/main/Main.ts
@@ -1,11 +1,14 @@
 import './main.scss';
 import createElement from '../../../utils/createElement';
+import Aside from './aside/Aside';
 
 export default class Main {
+  private aside: Aside = new Aside();
+
   public draw(): HTMLElement {
     const mainEl: HTMLElement = createElement({ tag: 'main' });
     const wrapperEl: HTMLElement = createElement({ tag: 'div', classes: ['wrapper'] });
-    const asideEl: HTMLElement = createElement({ tag: 'aside' });
+    const asideEl: HTMLElement = this.aside.draw();
 
     wrapperEl.appendChild(createElement({ tag: 'section', classes: ['pot'] }));
     wrapperEl.appendChild(createElement({ tag: 'section', classes: ['editor'] }));
---
+++ RSS-CSS-Selectors/src/components/view/main/aside/Aside.ts
@@ -0,0 +1,37 @@
+import './aside.scss';
+import createElement from '../../../../utils/createElement';
+import levels from '../../../../data/levels';
+
+export default class Aside {
+  public draw(): HTMLElement {
+    const asideEl: HTMLElement = createElement({ tag: 'aside' });
+    const title: HTMLElement = createElement({ tag: 'h6', content: 'Levels' });
+    const levelWrapperel: HTMLElement = createElement({ tag: 'div', classes: ['levels-wrapper'] });
+    const buttonEl: HTMLElement = createElement({ tag: 'button', classes: ['button'], content: 'Reset progress' });
+
+    const activeLevel = localStorage.getItem(`ily-active`) || 0;
+
+    asideEl.appendChild(title);
+
+    levels.forEach((lev, ind) => {
+      const classes = ['level-btn'];
+      const state = localStorage.getItem(`ily-level${ind}`);
+      if (state) {
+        classes.push(state);
+      }
+      if (+activeLevel === ind) {
+        classes.push('active');
+      }
+      const levelEl = createElement({ tag: 'button', classes: classes, content: `Level ${ind + 1}` });
+      levelWrapperel.appendChild(levelEl);
+
+      levelEl.addEventListener('click', () => console.log(`Level ${ind + 1}`));
+    });
+    asideEl.appendChild(levelWrapperel);
+
+    buttonEl.addEventListener('click', () => console.log(`Reset progress`));
+    asideEl.appendChild(buttonEl);
+
+    return asideEl;
+  }
+}
