baffc54168493b18baa7848295651264d9879f90
Illia Sakharau <sakharovia0204@gmail.com>
2023-07-17 21:11:37 +0400

    init: setting webpack, eslint etc

---
+++ async-race/eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "es2021": true
+    },
+    "extends": [
+        "eslint:recommended",
+        'airbnb',
+        'airbnb-typescript/base',
+        "plugin:@typescript-eslint/recommended",
+        'plugin:prettier/recommended'
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module",
+        "project": "./tsconfig.json",
+        "tsconfigRootDir": __dirname
+    },
+    "plugins": [
+        "@typescript-eslint",
+        'prettier'
+    ],
+    "rules": {
+        "@typescript-eslint/no-explicit-any": 'error',
+        "@typescript-eslint/no-inferrable-types": 'off',
+        "@typescript-eslint/explicit-member-accessibility": [
+            'error',
+            {
+                accessibility: 'explicit',
+                overrides: {
+                  accessors: 'explicit',
+                  constructors: 'off',
+                  methods: 'explicit',
+                  properties: 'explicit',
+                  parameterProperties: 'explicit',
+                },
+            }
+        ],
+        'prettier/prettier': 'error',
+    },
+    ignorePatterns: ['.eslintrc.js', 'webpack.config.js'],
+}
