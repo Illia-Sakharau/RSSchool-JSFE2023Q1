136388a1ee258fdbfae1107688547724d8797404
Illia Sakharau <sakharovia0204@gmail.com>
2023-07-20 22:28:33 +0400

    feat: add carView34 creater

--- async-race/src/index.ts
+++ async-race/src/index.ts
@@ -1,7 +1,4 @@
 import './styles/styles.scss';
 import { router } from './router/router';
 
-//delete
-document.body.innerHTML = `<h1>123</h1><a class="link" href="#garage">garage</a></br><a class="link" href="#winners">Winners</a>`;
-
 router();
